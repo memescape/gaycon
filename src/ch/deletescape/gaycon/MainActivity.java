dc963703b049510f81391e1d460d9133fd6e5e6d
Till Kottmann <deletescape@gmail.com>
2018-03-28 22:19:55 +0200

    Make changes for gaycon
    Haha, yes

---
+++ src/ch/deletescape/gaycon/MainActivity.java
@@ -0,0 +1,10 @@
+package ch.deletescape.gaycon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity
+{
+}
